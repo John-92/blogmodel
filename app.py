58767f29182e5bf0adfda0c1d7c0f42bb3c04a83
john <986737566@qq.com>
2022-08-28 23:49:33 +0800

    0828

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # @Author: John
 # @File: app.py
 # @Software: PyCharm
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, flash
 # from jinja2 import escape
 
 
@@ -35,25 +35,23 @@ def test(color):
 
 @app.route("/hello")
 def hello():
-    # name=request.args.get('name','Flask')
-    # name=request.args.get('name2','Flask')
-    # print(name)
-    # return f'{name}'
-    # return '',302,{'Location','www.baidu.com'}
-    # return redirect(request.referrer or url_for('hello_world'))
     print(request.args.get('next'))
     return redirect(request.args.get('next'))
 
 @app.route("/bar")
 def ext():
     return '<h1>next page</h1><a href="%s">sss</a>' %url_for('hello_world',next=request.full_path)
-    # print("hhh")
-    # return 'hhh'
 
 @app.route("/xss")
 def xss():
     name=request.args.get('name')
     return '<h1>next page %s</h1>' %escape(name)
+
+@app.route("/flash")
+def just_flash():
+    flash("i am flash")
+    return redirect(url_for('index'))
+    # return '<h1>next page %s</h1>' %escape(name)
 
 @app.route("/hi")
 def hi():
@@ -62,7 +60,19 @@ def hi():
     print(name)
     return f'{name}'
 
-
+@app.route("/jinja")
+def jinja():
+    #将变量渲染到模板上
+    user={}
+    user['username']='john'
+    user['bio']='jinja test'
+    movies=[
+        {'name':'helloworld','year':'1998'},
+        {'name':'helloworld2','year':'1999'},
+        {'name':'helloworld3','year':'1997'},
+        {'name':'helloworld4','year':'1996'}
+    ]
+    return render_template('jinja.html',user=user,movies=movies)
 
 @app.route("/404")
 def not_found():
@@ -72,3 +82,4 @@ def not_found():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    app.secret_key='129-726-914'
